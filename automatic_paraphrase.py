2121028bd8f02a0450759c428b1153cbdf71947d
Lukasz <lukasz@togedo.com>
2019-07-31 19:16:35 +0200

    Avoid plurals. Cleanup.

--- automatic_paraphrase.py
+++ automatic_paraphrase.py
@@ -1,34 +1,62 @@
 import spacy
 
+test_cases = [
+    "This tree looks beautiful!",
+    "I am very tired.",
+    "You should buy a new shirt.",
+    "Nancy ate 20 hot dogs yesterday.",
+]
+
+handled_poss = ["NOUN", "ADJ", 'ADV']
+
 spacy.prefer_gpu()
 nlp = spacy.load("en_core_web_lg")
 
+def get_lemma_from_lexeme(lexeme):
+    return nlp(lexeme.orth_)[0].lemma
+
+def avoid_token_plurals_in_lexemes(lexemes, token):
+    return filter( lambda x: get_lemma_from_lexeme(x) != token.lemma, lexemes )
+
+def filter_lexemes_by_similarity_to_token(lexemes, token, treshold):
+    return filter( lambda x: x.similarity(token) > treshold, lexemes )
+
 def same_cluster(word):
     queries = [w for w in word.vocab if w.is_lower == word.is_lower and w.prob >= -15]
-    # by_cluster = filter(lambda x: x.cluster == word.cluster, queries)
     return queries
 
 def most_similar(word):
     cluster = same_cluster(word)
     by_similarity = sorted(cluster, key=lambda w: word.similarity(w), reverse=True)
-    # print([w for w in map(lambda x: [x.orth_,x.similarity(word)],by_similarity[:10])])
-    return [w.orth_ for w in filter(lambda x: x.similarity(word) > 0.7,by_similarity[:10])]
-
-handled_poss = ["NOUN", "ADJ", 'ADV']
+    return [w.orth_ for w in avoid_token_plurals_in_lexemes(filter_lexemes_by_similarity_to_token(by_similarity[:10], word, 0.5), word)]
 
 def get_synonim(token):
     if token.pos_ in handled_poss:
         synonim = next((x for x in most_similar(token) if x != token.orth_), False)
-        if synonim:
+        synonim_doc = nlp(synonim) # Avoid plurals
+        synonim_token = synonim_doc[0]
+        if synonim and synonim_token.lemma != token.lemma:
             return synonim
-        return token.orth_
-    return token.orth_
+    return token.orth_ # If nothing found return the same token
 
 def get_paraphrase(sentence):
     doc = nlp(sentence)
-    return ' '.join(map(get_synonim, doc))
+    para = ' '.join(map(get_synonim, doc))
+    para_doc = nlp(para)
+    # print("Paraphrase similarity by document:", para_doc.similarity(doc))
+    return para
 
 
 while True:
+    test_results = []
+
+    for test in test_cases:
+        test_results.append([test, get_paraphrase(test)])
+
+    print("Test cases")
+    for result in test_results:
+        print("Case: ", result[0])
+        print("Result: ", result[1])
+    
     sent = input('Give me a sentence to paraphrase:   ')
     print(get_paraphrase(sent))
